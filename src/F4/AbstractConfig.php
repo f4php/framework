65b5727051332c0b3b4ec6a6b1008508288dc422
Dennis Kreminsky <dennis@kreminsky.com>
2024-10-22 22:52:39 +0300

    Commented out unimplemented class configs, fixed session-related type in AbstractConfig

--- src/F4/AbstractConfig.php
+++ src/F4/AbstractConfig.php
@@ -4,13 +4,13 @@ declare(strict_types = 1);
 
 namespace F4;
 
-use F4\DB\Adapter\PostgreSQL;
-use F4\DB\Adapter\PostgreSQL\TypeCaster;
+// use F4\DB\Adapter\PostgreSQL;
+// use F4\DB\Adapter\PostgreSQL\TypeCaster;
 // use F4\Phug\Renderer as PhugTemplateRenderer;
 
-use F4\Core\Dispatcher;
-use F4\Core\Localizer;
-use F4\Core\Router;
+// use F4\Core\Dispatcher;
+// use F4\Core\Localizer;
+// use F4\Core\Router;
 
 abstract class AbstractConfig
 {
@@ -31,10 +31,10 @@ abstract class AbstractConfig
     const ?string DB_APP_NAME = null;
     const bool DB_TRACE = false;
     const bool DB_ALLOW_UNSAFE_RAW_QUERIES = false;
-    const string DB_ADAPTER_CLASS = PostgreSQL::class;
+    // const string DB_ADAPTER_CLASS = PostgreSQL::class;
     const string DB_ADAPTER_ALIAS = '\F4\DB';
 
-    const string DB_TYPE_CASTER_CLASS = TypeCaster::class;
+    // const string DB_TYPE_CASTER_CLASS = TypeCaster::class;
 
     const bool DB_PERSIST = true;
     const bool DB_KEEP_NULLS = true;
@@ -55,23 +55,23 @@ abstract class AbstractConfig
     const string DEFAULT_OUTPUT_FORMAT = 'html'; // renderer must be set in OUTPUT_RENDERERS
 
     const array OUTPUT_RENDERERS = [
-        'html'      => Core\Renderer\Html::class,
-        'json'      => Core\Renderer\Json::class,
-        'txt'       => Core\Renderer\Text::class,
-        'text'      => Core\Renderer\Text::class,
+        // 'html'      => Core\Renderer\Html::class,
+        // 'json'      => Core\Renderer\Json::class,
+        // 'txt'       => Core\Renderer\Text::class,
+        // 'text'      => Core\Renderer\Text::class,
         // 'xml'      => Core\Renderer\Xml::class,
         // these are special formats used for development perposes only, their availability depends on debug mode setting
-        'raw'       => self::DEBUG_MODE ? Core\Renderer\DebugRaw::class : null,
-        'routes'    => self::DEBUG_MODE ? Core\Renderer\DebugRoutes::class : null,
-        'trace'     => self::DEBUG_MODE ? Core\Renderer\DebugTrace::class : null,
-        'test'      => self::DEBUG_MODE ? Core\Renderer\DebugTest::class : null
+        // 'raw'       => self::DEBUG_MODE ? Core\Renderer\DebugRaw::class : null,
+        // 'routes'    => self::DEBUG_MODE ? Core\Renderer\DebugRoutes::class : null,
+        // 'trace'     => self::DEBUG_MODE ? Core\Renderer\DebugTrace::class : null,
+        // 'test'      => self::DEBUG_MODE ? Core\Renderer\DebugTest::class : null
     ];
 
     const string SESSION_COOKIE_NAME = 'f4';
     const ?string SESSION_DOMAIN = null;
     const ?string SESSION_HANDLER = null;
     const bool SESSION_HTTP_ONLY = false;
-    const string SESSION_REQUIRED = true;
+    const bool SESSION_REQUIRED = true;
     const string SESSION_LIFETIME = '';
     const string SESSION_PATH = '/';
     const string SESSION_SAME_SITE = 'Strict';
@@ -83,9 +83,9 @@ abstract class AbstractConfig
     const array DICTIONARIES = [];
     const string DEFAULT_LANGUAGE = 'en';
 
-    const string CORE_DISPATCHER_CLASS = Dispatcher::class;
-    const string CORE_ROUTER_CLASS = Router::class;
-    const string CORE_LOCALIZER_CLASS = Localizer::class;
+    // const string CORE_DISPATCHER_CLASS = Dispatcher::class;
+    // const string CORE_ROUTER_CLASS = Router::class;
+    // const string CORE_LOCALIZER_CLASS = Localizer::class;
 
     const bool APPEND_FORMAT_SUFFIX_ON_REDIRECT = true;
 

d501035003b1545e51b3a2b3727ef42cf01b9df2
Dennis Kreminsky <dennis@kreminsky.com>
2024-10-22 18:57:30 +0300

    Loader fix

--- src/F4/Loader.php
+++ src/F4/Loader.php
@@ -6,12 +6,12 @@ use Composer\Script\Event;
 
 class Loader {
   
-  public function loadConfigurationFile($environment) {
+  public static function loadConfigurationFile($environment) {
     $composerConfiguration = json_decode(json: file_get_contents(filename: __DIR__."../../../../composer.json"), associative: true, flags: JSON_THROW_ON_ERROR);
     echo $composerConfiguration["extra"]["F4"]["configs"][$_SERVER["ENVIRONMENT"]??'default'] ?? null;
   }
 
-  public function createConfigurationFile(Event $event, ?string $filename=null) {
+  public static function createConfigurationFile(Event $event, ?string $filename=null) {
     var_dump($event->getArguments());
     
   }

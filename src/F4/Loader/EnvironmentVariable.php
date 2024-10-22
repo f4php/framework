d91fb34c340c3391aeb24e6014ec1ed8639b2b7a
Dennis Kreminsky <dennis@kreminsky.com>
2024-10-22 23:59:33 +0300

    Attribute-related syntax fixes

--- src/F4/Loader/EnvironmentVariable.php
+++ src/F4/Loader/EnvironmentVariable.php
@@ -2,6 +2,9 @@
 
 namespace F4\Loader;
 
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS_CONSTANT)]
 class EnvironmentVariable extends ConfigAttribute {
 
   public function __construct(?string $environmentVariableName=null) {
--- src/F4/Loader/SensitiveParameter.php
+++ src/F4/Loader/SensitiveParameter.php
@@ -2,6 +2,9 @@
 
 namespace F4\Loader;
 
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS_CONSTANT)]
 class SensitiveParameter extends ConfigAttribute {
 
 }

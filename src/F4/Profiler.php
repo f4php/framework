d42392d90adb81f880cc49ed9459a956b083d5e0
Dennis Kreminsky <dennis@kreminsky.com>
2025-02-09 14:10:11 +0200

    Changed namespace location of HookManager and Profiler

--- src/F4/Profiler.php
+++ src/F4/Profiler.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace F4\Core;
+namespace F4;
 
 use function hrtime;
 use function memory_get_usage;
@@ -14,10 +14,11 @@ class Profiler
     public static function init(): void {
         self::addSnapshot('App init');
     }
-    public static function addSnapshot(string $description): void {
+    public static function addSnapshot(?string $label = null, ?string $description = null): void {
         self::$snapshots[] = [
             'memory' => memory_get_usage(real_usage: false),
             'time' => hrtime(as_number: true),
+            'label' => $label,
             'description' => $description
         ];
     }
